09acf8ce650dc242d6cae6c0d514e3f73e043541
Iseg6ix <freddyjseg@gmail.com>
2023-03-14 17:34:55 -0700

    Task" "

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-//This script prints an output depending on the arguments passed
-
-if (process.argv.length === 2){
-	console.log("No arguments);
-} else if (process.argv.length === 3){
-	console.log("Argument found);
-} else{
-	console.log("Arguments found");
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
